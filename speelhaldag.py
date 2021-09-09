49765ebe56eb6bf120d8776bd5d58e1e2c137266
Justin <99066957@mydavinci.nl>
2021-09-09 10:16:21 +0200

    files added

---
+++ speelhaldag.py
@@ -0,0 +1,15 @@
+# speelhaldag.py
+
+print("7 * 7.45")
+print("= 22.35")
+
+print("5 * 9")
+print("= 45")
+
+print("(0.37 * 9) * 4")
+print("= 13.32")
+
+print("22.35 + 13.32")
+print("= 35.67")
+
+print("Dit geweldige dagje-uit met 4 mensen in de Speelhal met 45 minuten VR kost je maar 35.67 euro!")
